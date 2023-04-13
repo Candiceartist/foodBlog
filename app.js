1efdb16116fbeadb070773fe234ad56c06c0e14f
Candice Jovon Juarez <candice.artist@gmail.com>
2023-04-13 09:26:58 -0400

    added port and congfig

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ const express = require('express');
 const expresslayouts = require('express-ejs-layouts');
 
 const app = express();
+const port = process.env.Port || 3000;
+
+require('dotenv').config();
