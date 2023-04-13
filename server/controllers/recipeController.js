3bd554b036f6a8e033bee2b4c5bbe24fcb60308c
Candice Jovon Juarez <candice.artist@gmail.com>
2023-04-13 18:32:37 -0400

    added categories page view and route

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -13,3 +13,15 @@ exports.homepage = async(req, res) => {
         res.status(500).send({message: error.message || "Error Occured"});
     }
 }
+
+exports.exploreCategories = async(req, res) => {
+    try {
+    const limitNumber = 20;
+    const categories = await Category.find({}).limit(limitNumber);
+
+
+    res.render('categories', {title:"Good Eats-Food Blog Categories", categories});
+    } catch (error) {
+        res.status(500).send({message: error.message || "Error Occured"});
+    }
+}
