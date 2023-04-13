d2bc66c7dc4afeef0cee70c069bd119cdf196783
Candice Jovon Juarez <candice.artist@gmail.com>
2023-04-13 09:34:29 -0400

    added middleware

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
-const expresslayouts = require('express-ejs-layouts');
+const expressLayouts = require('express-ejs-layouts');
+
 
 const app = express();
 const port = process.env.Port || 3000;
 
 require('dotenv').config();
+
+// middleware
+app.use(express.urlencoded( {extended:true} ));
+// public folder to shorten path to object
+app.use(express.static('public'));
+app.use(expressLayouts);
+
+app.set('layout', './layouts/main')
