7b3946a819fda3e19bfafe3e9ea95c4352a44dec
Candice Jovon Juarez <candice.artist@gmail.com>
2023-04-16 12:35:53 -0400

    category linked to recipe sort

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -34,7 +34,7 @@ exports.exploreCategoriesById = async(req, res) => {
     try {
     let categoryId = req.params.id;
     const limitNumber = 20;
-    const categoryById = await Category.find({'category': categoryId}).limit(limitNumber);
+    const categoryById = await Recipe.find({'category': categoryId}).limit(limitNumber);
     res.render('categories', {title:"Good Eats-Food Blog Categories", categoryById});
     } catch (error) {
         res.status(500).send({message: error.message || "Error Occured"});
