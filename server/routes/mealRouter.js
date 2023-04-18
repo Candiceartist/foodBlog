1078d593f58ce06161fff4aaf275d6907770f97c
Candice Jovon Juarez <candice.artist@gmail.com>
2023-04-18 13:45:32 -0400

    new ejs page for meal api

--- server/routes/mealRouter.js
+++ server/routes/mealRouter.js
@@ -9,14 +9,18 @@ const axios = require('axios')
 mealRouter.get('', async(req, res) => {
     try {
      const newAPI = await axios.get(`https://www.themealdb.com/api/json/v1/1/random.php`)
-     console.log(newAPI.data)
+     res.render('new',{ meals : newAPI.data })
 
     } catch (error) {
         if(error.response) {
             console.log(err.response.data)
-            console.log(err.response.data)
-            console.log(err.response.data)
-            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.headers)
+
+        } else if( err.request) {
+          console.log(err.request)
+        } else {
+            console.error('Error', err.message)
         }
 
     }
